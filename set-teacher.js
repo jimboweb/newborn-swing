f773798ad58ccc2f98be3d17862f93b0adba4c28
Jim Stewart <jim.stewart@thelangschool.org>
2026-05-12 15:08:47 -0400

    remove temp set-teacher script

--- set-teacher.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-const pool = require('./db');
-pool.query("UPDATE users SET role = 'teacher' WHERE email = 'jim.stewart@thelangschool.org'")
-  .then(() => { console.log('done'); pool.end(); })
-  .catch(err => { console.error(err.message); pool.end(); });
