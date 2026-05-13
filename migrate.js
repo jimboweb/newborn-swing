c5a7dee848e7b54c88a355f8e037a536c3b059a2
Jim Stewart <jim.stewart@thelangschool.org>
2026-05-13 13:55:35 -0400

    remove migrate.js

--- migrate.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-const pool = require('./db');
-pool.query("ALTER TABLE problems ADD COLUMN IF NOT EXISTS starter_code TEXT NOT NULL DEFAULT ''")
-  .then(() => { console.log('done'); pool.end(); })
-  .catch(err => { console.error(err.message); pool.end(); });
